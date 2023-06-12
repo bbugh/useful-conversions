c65462083e98c1fc7d6efee3d0557162ca1c8f7e
Brian Bugh <438465+bbugh@users.noreply.github.com>
2023-06-12 15:15:02 -0500

    Working first version

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,65 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, ref } from 'vue'
+import * as data from './data.json'
+
+import configureMeasurements, { allMeasures, type AllMeasuresUnits } from 'convert-units'
+const convert = configureMeasurements(allMeasures)
+
+const allRealUnits = convert().list()
+
+type RealUnit = keyof typeof data
+
+const funnyUnits = computed(() =>
+  selectedRealType.value
+    ? data[selectedRealType.value as RealUnit] // this is a bad casting, it could be other types
+    : []
+)
+
+const realNumber = ref(150)
+const selectedRealUnit = ref<AllMeasuresUnits>()
+const selectedFunnyUnit = ref<AllMeasuresUnits>()
+
+const selectedRealType = computed(() =>
+  selectedRealUnit.value
+    ? allRealUnits.find((unit) => unit.abbr === selectedRealUnit.value)?.measure
+    : undefined
+)
+
+const selectedFunnyType = computed(() =>
+  selectedRealType.value && selectedFunnyUnit.value
+    ? data[selectedRealType.value as RealUnit]?.find((unit) => unit.funnyUnit === selectedFunnyUnit.value)
+    : undefined
+)
+
+const funnyCount = computed(() =>
+  selectedRealUnit.value && selectedFunnyType.value
+    ? convert(realNumber.value).from(selectedRealUnit.value).to(selectedFunnyType.value.realUnit as AllMeasuresUnits) / selectedFunnyType.value.conversionFactor
+    : 0
+)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div style="display: flex; flex-direction: row">
+      <input v-model="realNumber" type="number" />
+      <select v-model="selectedRealUnit">
+        <option v-for="realUnit in allRealUnits" :key="realUnit.abbr" :value="realUnit.abbr">
+          {{ realUnit.plural }} ({{ realUnit.abbr }}) [{{ realUnit.measure }}]
+        </option>
+      </select>
+      TO
+      <select v-model="selectedFunnyUnit">
+        <option
+          v-for="funnyUnit in funnyUnits"
+          :key="funnyUnit.funnyUnit"
+          :value="funnyUnit.funnyUnit"
+        >
+          {{ funnyUnit.funnyUnit }}
+        </option>
+      </select>
+    </div>
+
+    <div>{{ realNumber }} {{ selectedRealUnit }} = about {{ funnyCount }} {{ selectedFunnyUnit }}</div>
   </main>
 </template>
 
