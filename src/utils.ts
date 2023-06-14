4a8e39005e1e8e5922e0c78c06cbc479d5acf3da
Brian Bugh <438465+bbugh@users.noreply.github.com>
2023-06-14 07:37:11 -0500

    Fix very small number formatting

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 // Things that should part of the standard library but aren't
 
-export const formatNumber = (num: number) => (num >= 1 ? new Intl.NumberFormat().format(num) : num)
+export const formatNumber = (num: number) =>
+  new Intl.NumberFormat(undefined, { maximumSignificantDigits: 3 }).format(num)
 
 export const groupBy = <T, K extends keyof any>(arr: T[], key: (i: T) => K) =>
   arr.reduce((groups, item) => {
